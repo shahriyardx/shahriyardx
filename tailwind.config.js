ccd5a8e50c2eee73d0c3e1430c58967c3f4d78b1
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-04-27 18:47:42 +0600

    Project setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.js",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2D2E32",
+        secondary: "#25262A",
+        accent: "#6DF6A3"
+      },
+      fontFamily: {
+        "sourcecode": ['Source Code Pro', 'monospace'],
+        "montserrat": ['Montserrat', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
