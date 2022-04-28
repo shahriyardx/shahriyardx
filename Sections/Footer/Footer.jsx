130f47a178cea22c1a349a70f4edc72f21b9c006
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-04-28 15:24:32 +0600

    Added footer

---
+++ Sections/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Container from '../../components/Layout/Container'
+
+const Footer = () => {
+  return (
+    <div className='bg-zinc-900 py-20'>
+      <Container className='flex justify-between flex-col md:flex-row gap-5'>
+        <h1 className='text-4xl font-black uppercase flex gap-1 text-zinc-300 justify-center'>
+          <span>Shahriyar</span>
+          <span className='font-thin'>Alam</span>
+        </h1>
+
+        <p className='text-zinc-500 text-lg tracking-tighter text-center'>Copythight &copy; All Rights Reserved.</p>
+      </Container>
+    </div>
+  )
+}
+
+export default Footer
