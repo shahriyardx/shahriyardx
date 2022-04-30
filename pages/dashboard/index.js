3e2f2523596258221b3db130588edca455e95bfe
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-01 01:39:36 +0600

    Dynamic project count

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import CountCart from '../../components/Dashboard/CountCart/CountCart'
 import DashLayout from '../../components/Layout/DashLayout'
 
 const Dashboard = () => {
+  const projects = useSelector(state => state.projects.value)
   return (
     <DashLayout>
       <div className='grid grid-cols-1 sm:grid-cols-4 gap-5'>
         <CountCart text="Posts" count={0} />
-        <CountCart text="Projects" count={6} />
+        <CountCart text="Projects" count={projects.length} />
       </div>
     </DashLayout>
   )
