2605214cb7cfec7447618caa4cd171bcccd61e15
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-01 00:21:24 +0600

    Added production api

--- utils/api.js
+++ utils/api.js
@@ -1,1 +1,1 @@
-export const API_BASE=process.env.NODE_ENV == "development" ? 'http://localhost:3000/api' : ''
+export const API_BASE=process.env.NODE_ENV == "development" ? 'http://localhost:3000/api' : 'https://shahriyar.dev/api'
