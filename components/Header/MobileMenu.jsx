10d85ec29b488dc97cd8a4a6d52e4357d6519c71
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-04 20:04:59 +0600

    Added logout button for mobile

--- components/Header/MobileMenu.jsx
+++ components/Header/MobileMenu.jsx
@@ -7,13 +7,19 @@ import {
   AiFillMail, 
   AiFillFacebook 
 } from 'react-icons/ai'
+import { signOut, useSession } from 'next-auth/react'
 
 const MobileMenu = () => {
+  const {data: session} = useSession()
+
   return (
     <div className={`absolute top-16 left-0 w-full bg-secondary lg:hidden z-10 py-5`}>
       <Container className='px-4'>
         <TextLink href="/" className='py-2'>Home</TextLink>
         <TextLink href="/blog" className='py-2'>Blog</TextLink>
+        {session?.user && (
+          <button type='button' onClick={signOut} className='text-red-400 hover:text-red-500 py-2'>Logout</button>
+        )}
         {/* <TextLink href="/contact" className='py-2'>Contact</TextLink> */}
 
         <div className='py-2 pb-5 flex gap-3 text-3xl'>
