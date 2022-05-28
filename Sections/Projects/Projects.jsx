5bff87defd6df2daa30342a65ccdb58f54f98a10
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-05-29 01:12:42 +0600

    Show all button

--- Sections/Projects/Projects.jsx
+++ Sections/Projects/Projects.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import Container from '../../components/Layout/Container'
-import Project from '../../components/Project/Project'
+import React, { useState } from "react";
+import Container from "../../components/Layout/Container";
+import Project from "../../components/Project/Project";
 
 const Projects = ({ projects }) => {
+  const [showAll, setShowAll] = useState(false);
+  const renderRrojects = showAll ? projects : projects.slice(0, 6);
   return (
-    <div className='bg-primary'>
-      <Container className='py-10 md:py-20'>
-        <h1 className='text-4xl font-black tracking-tighter text-zinc-300 text-center mb-10'>Projects</h1>
+    <div className="bg-primary">
+      <Container className="py-10 md:py-20">
+        <h1 className="text-4xl font-black tracking-tighter text-zinc-300 text-center mb-10">
+          Projects
+        </h1>
 
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10'>
-          {projects.slice(0, 6).map(project => {
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10">
+          {renderRrojects.map((project) => {
             return (
               <Project
                 key={project._id}
@@ -17,14 +21,23 @@ const Projects = ({ projects }) => {
                 url={project.url}
                 description={project.description}
                 image={project.image}
-                tags={project.tags.map(tag => tag.value)}
+                tags={project.tags.map((tag) => tag.value)}
               />
-            )
+            );
           })}
+        </div>
+
+        <div
+          className="mt-5 flex justify-center"
+          onClick={() => setShowAll(!showAll)}
+        >
+          <button className="px-5 py-3 text-white bg-zinc-600">
+            {showAll ? "Show Less" : "Show All"}
+          </button>
         </div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
--- utils/schemas/Project.js
+++ utils/schemas/Project.js
@@ -1,11 +1,15 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const ProjectSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  url: String,
-  image: String,
-  tags: [{value: String, label: String}],
-})
+const ProjectSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    url: String,
+    image: String,
+    tags: [{ value: String, label: String }],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.models.Project || mongoose.model("Project", ProjectSchema)
+module.exports =
+  mongoose.models.Project || mongoose.model("Project", ProjectSchema);
