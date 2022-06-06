cfd47b3a0d992d134350bbcba888d964199b5a4b
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-06-06 18:30:57 +0600

    Added cv

--- next.config.js
+++ next.config.js
@@ -2,8 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com"]
+    domains: ["res.cloudinary.com"],
   },
-}
+  async redirects() {
+    return [
+      {
+        source: "/cv",
+        destination: "/cv.pdf",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,26 @@ module.exports = {
   content: [
     "./pages/**/*.js",
     "./components/**/*.{js,jsx}",
-    "./Sections/**/*.{js,jsx}"
+    "./Sections/**/*.{js,jsx}",
   ],
   theme: {
     extend: {
       gridTemplateColumns: {
-        dashboard: '300px auto',
-        cards: ''
+        dashboard: "300px auto",
       },
       screens: {
-        'msm': '400px',
+        msm: "400px",
       },
       colors: {
         primary: "#2D2E32",
         secondary: "#25262A",
-        accent: "#6DF6A3"
+        accent: "#6DF6A3",
       },
       fontFamily: {
-        "sourcecode": ['Source Code Pro', 'monospace'],
-        "montserrat": ['Montserrat', 'sans-serif']
-      }
+        sourcecode: ["Source Code Pro", "monospace"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-}
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
