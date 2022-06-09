e23ca546c5016dca19f0c887f1f2c591a7eb7a8e
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-06-10 03:07:26 +0600

    Added new projects

--- components/Tag/Tag.jsx
+++ components/Tag/Tag.jsx
@@ -1,48 +1,52 @@
-import React from 'react'
+import React from "react";
 
 const tagMap = {
   HTML: {
-    bg: 'bg-[#F53900]/20',
-    text: 'text-[#F53900]'
+    bg: "bg-[#F53900]/20",
+    text: "text-[#F53900]",
   },
   CSS: {
-    bg: 'bg-[#2862E9]/20',
-    text: 'text-[#2862E9]'
+    bg: "bg-[#2862E9]/20",
+    text: "text-[#2862E9]",
   },
   JavaScript: {
-    bg: 'bg-[#F7DF1E]/10',
-    text: 'text-[#F7DF1E]'
+    bg: "bg-[#F7DF1E]/10",
+    text: "text-[#F7DF1E]",
   },
   Python: {
-    bg: 'bg-[#a974f9]/10',
-    text: 'text-[#f4bc84]'
+    bg: "bg-[#a974f9]/10",
+    text: "text-[#f4bc84]",
   },
   ReactJs: {
-    bg: 'bg-[#85D8F8]/10',
-    text: 'text-[#85D8F8]'
+    bg: "bg-[#85D8F8]/10",
+    text: "text-[#85D8F8]",
   },
   NodeJs: {
-    bg: 'bg-[#75AC64]/20',
-    text: 'text-[#75AC64]'
+    bg: "bg-[#75AC64]/20",
+    text: "text-[#75AC64]",
+  },
+  ExpressJs: {
+    bg: "bg-[#75AC64]/20",
+    text: "text-[#75AC64]",
   },
   NextJs: {
-    bg: 'bg-black',
-    text: 'text-white'
+    bg: "bg-black",
+    text: "text-white",
   },
   TailwindCss: {
-    bg: 'bg-[#0EA5E9]/20',
-    text: 'text-[#0EA5E9]'
-  }
-}
+    bg: "bg-[#0EA5E9]/20",
+    text: "text-[#0EA5E9]",
+  },
+};
 
 const Tag = (props) => {
-  const { bg: bgColor, text: textColor } = tagMap[props.text]
-  const styles = `${bgColor} ${textColor}`
+  const { bg: bgColor, text: textColor } = tagMap[props.text];
+  const styles = `${bgColor} ${textColor}`;
   return (
     <div className={`px-2 py-1 rounded-lg text-sm ${styles} cursor-pointer`}>
       # {props.text}
     </div>
-  )
-}
+  );
+};
 
-export default Tag
+export default Tag;
--- data/projects.js
+++ data/projects.js
@@ -39,7 +39,7 @@ const projects = [
   {
     name: "Compart",
     slug: "compart",
-    tags: ["ReactJs", "TailwindCss", "NodeJs", "JavaScript"],
+    tags: ["ReactJs", "TailwindCss", "NodeJs", "JavaScript", "ExpressJs"],
     description:
       "Pc parts manufaturer website. Where manufaturers can login and add products",
     links: [
@@ -58,6 +58,71 @@ const projects = [
     ],
     technologies:
       "ReactJs, TailwindCss, NodeJs, JWT Authentication, Firebase, MongoDB, Stripe",
+  },
+  {
+    name: "Wildie",
+    slug: "wildie",
+    tags: ["ReactJs", "TailwindCss", "NodeJs"],
+    description: "Wild life photographer portfolio and service website",
+    links: [
+      { text: "Live Site", url: "https://wildie-shahriyardx.web.app/" },
+      { text: "Repository", url: "https://github.com/shahriyardx/wildie" },
+    ],
+    features: [
+      "React Js for UI Management",
+      "React Router Dom for routes",
+      "Twilwindcss for UI Design",
+      "Firebase for authentication",
+      "Firebase hosting to deploy site to public",
+    ],
+    technologies: "ReactJs, TailwindCss, NodeJs, JWT Authentication, Firebase",
+  },
+  {
+    name: "Makeown",
+    slug: "makeown",
+    tags: ["HTML", "CSS", "JavaScript", "Python"],
+    description:
+      "A discord bot dashboard to manage the bot from a web interface",
+    links: [
+      { text: "Live Site", url: "https://makeown.cc" },
+      { text: "Repository (Closed Source)", url: "#" },
+    ],
+    features: [
+      "Login with discord",
+      "Responsive Layout",
+      "Backend API with python",
+      "Postgresql Database using asyncpg (python)",
+      "Hosted on a Linux VPS",
+      "Inter-process communication using websockets",
+    ],
+    technologies: "Python, Websockets, Postgresql, Quart (Framework)",
+  },
+  {
+    name: "Laptop Carnival",
+    slug: "laptopcarnival",
+    tags: ["ReactJs", "TailwindCss", "NodeJs", "ExpressJs"],
+    description:
+      "Best inventory for selling laptops. Worldwide shipping, fastest delivery",
+    links: [
+      { text: "Live Site", url: "https://laptop-carnival.web.app/" },
+      {
+        text: "Frontend",
+        url: "https://github.com/shahriyardx/laptop-carnival",
+      },
+      {
+        text: "Backend",
+        url: "https://github.com/shahriyardx/laptop-carnival-backend",
+      },
+    ],
+    features: [
+      "ReactJs library for UI",
+      "Tailwindcss for designing the frontend",
+      "Powered by nodejs backend api hosted on heroku",
+      "Login with email/password and google",
+      "JWT Token Authentication",
+      "Inter-process communication using websockets",
+    ],
+    technologies: "ReactJs, TailwindCSS, Firebase, MongoDB, jsobwebtoken",
   },
 ];
 
