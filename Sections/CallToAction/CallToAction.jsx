f3e08cc7ac7c281899033d29f8d6d840e56360da
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-06-10 04:13:06 +0600

    Added hover effect on hire me button

--- Sections/CallToAction/CallToAction.jsx
+++ Sections/CallToAction/CallToAction.jsx
@@ -7,7 +7,7 @@ const CallToAction = () => {
       <Container className="py-20 z-10">
         <div className="p-10 rounded-2xl text-center">
           <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-zinc-300 font-montserrat">
-            I know your mind wants to hire me 😁
+            I know your mind wants to hire me 👀
           </h1>
           <p className="text-zinc-400 text-sm sm:text-base mt-2">
             The button bellow will make it easy
@@ -16,11 +16,13 @@ const CallToAction = () => {
           <div className="mt-10">
             <a
               href="https://www.linkedin.com/in/shahriyardx/"
-              className="px-5 py-3 bg-accent text-black font-bold"
+              className="px-5 py-3 bg-accent text-black font-bold group"
               target="_blank"
               rel="noreferrer"
             >
-              Hire Me
+              <span className="mr-2">Hire Me</span>
+              <span className="group-hover:hidden">💔</span>
+              <span className="hidden group-hover:inline-block">💖</span>
             </a>
           </div>
         </div>
