f7be4a488bcb462887a8a1f9ffd425bb88e0f0c8
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-06-10 22:18:38 +0600

    Added about page

---
+++ pages/about.js
@@ -0,0 +1,57 @@
+import Main from "@/components/Layout/Main";
+import React from "react";
+
+const about = () => {
+  return (
+    <Main>
+      <div className="prose prose-invert prose-green py-10 mx-auto px-4">
+        <h2>Welcome 💘</h2>
+        <p className="text-zinc-400">
+          I am glad that you want to know about me 🥰. I&apos;m `Shahriyar`.
+          Full-stack web developer, Student of Computer Software Engineer, Tech
+          enthusiast and much more.
+        </p>
+
+        <h2>Story</h2>
+        <div className="text-zinc-400">
+          <p>
+            In my childhood, I wanted to be a pilot. Because I used to see a lot
+            of science fiction movies and seeing those I thought I can also fly
+            if I become a pilot
+          </p>
+
+          <p>
+            Later, when I was 11 years old. I suddenly started loving Math and
+            Science. I was soo good at match and science that all my friends
+            started calling me scientist
+          </p>
+
+          <p>
+            And they kept calling me that till my High school ended. It was a
+            very long time. At that time as a Bangladeshi citizen getting access
+            to technology was too hard. Like I had my first smartphone when I
+            was in class 10 (17 years old). Before that my Aunt bought a Chinese
+            phone. I used that to play games and stuff.
+          </p>
+
+          <p>
+            After playing a lot of games I was bored and thought why not make my
+            own games. But I didn&apos;t know how to make my own game. I
+            searched on the internet and I found nothing. Then I thought, ok
+            let&apos;s make mobile apps. I searched for how to make mobile
+            games. And I found AppGeyser where you can a template give it a name
+            and your app is done. Was too easy but not what I wanted.
+          </p>
+
+          <p>
+            Then I started learning HTML, CSS, and a little bit of Jquery. And I
+            made my first web page. That was the beginning and now I&apos; a
+            Full-stack web developer and a Python developer.
+          </p>
+        </div>
+      </div>
+    </Main>
+  );
+};
+
+export default about;
