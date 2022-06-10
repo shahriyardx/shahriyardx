37d3f7976cb4daa36b4df5bf6d62d485ebb19650
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-06-10 18:39:13 +0600

    Span 2 col on medium screen

--- components/Service/Service.jsx
+++ components/Service/Service.jsx
@@ -9,7 +9,7 @@ const Service = ({ title, content, icon, position }) => {
       whileInView={{ y: 0 }}
       viewport={{ once: true }}
       transition={{ duration: position * 0.2 }}
-      className="bg-zinc-700 p-10 rounded-sm"
+      className="bg-zinc-700 p-10 rounded-sm sm:last:col-span-2 md:last:col-span-1"
     >
       <Icon className="text-accent text-5xl" />
       <h1 className="text-2xl font-bold text-zinc-200 mt-2">{title}</h1>
