b42462b1fbfc86b2c747d5f45c48e9b51334a769
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-06-10 21:40:17 +0600

    Removes project routes

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import CountCart from '@/components/Dashboard/CountCart/CountCart'
-import DashLayout from '@/components/Layout/DashLayout'
-import SEO from '@/components/SEO'
+import React from "react";
+import { useSelector } from "react-redux";
+import CountCart from "@/components/Dashboard/CountCart/CountCart";
+import DashLayout from "@/components/Layout/DashLayout";
+import SEO from "@/components/SEO";
 
 const Dashboard = () => {
-  const projects = useSelector(state => state.projects.value)
-  const posts = useSelector(state => state.posts.value)
+  const projects = useSelector((state) => state.projects);
+  const posts = useSelector((state) => state.posts.value);
 
   return (
     <DashLayout>
-      <SEO title='Dashboard' />
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-4 gap-5'>
+      <SEO title="Dashboard" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-4 gap-5">
         <CountCart text="Posts" count={posts.length} />
         <CountCart text="Projects" count={projects.length} />
       </div>
     </DashLayout>
-  )
-}
+  );
+};
 
-Dashboard.requireAuth = true
+Dashboard.requireAuth = true;
 
-export default Dashboard
+export default Dashboard;
