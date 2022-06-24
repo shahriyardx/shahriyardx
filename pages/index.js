cffe84b63c13635f6914040ad52856847ed59dcf
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2022-06-24 11:03:42 +0600

    Fix sections import

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ import Main from "@/components/Layout/Main";
 import SEO from "@/components/SEO";
 import Services from "@/sections/Services/Services";
 import Banner from "@/sections/Banner/Banner";
-import Projects from "sections/Projects/Projects";
+import Projects from "@/sections/Projects/Projects";
 import projects from "../data/projects";
-import Skills from "sections/Skills/Skills";
-import CallToAction from "sections/CallToAction/CallToAction";
+import Skills from "@/sections/Skills/Skills";
+import CallToAction from "@/sections/CallToAction/CallToAction";
 
 const Home = () => {
   return (
