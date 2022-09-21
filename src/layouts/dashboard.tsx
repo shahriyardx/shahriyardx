91b3ad3463a04db1f485ad972a7deb50fe6152bb
shahriyardx <mdshahriyaralam552@gmail.com>
2022-09-22 03:50:29 +0600

    add: posts page

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -1,13 +1,17 @@
 import Sidebar from "components/dashboard/Sidebar/Sidebar"
 import React from "react"
 
-type Props = { children: React.ReactElement | React.ReactElement[] }
+type Props = {
+  children: React.ReactElement | React.ReactElement[]
+}
 
 const Dashboard = ({ children }: Props) => {
   return (
     <div className="grid grid-cols-dashboard font-montserrat">
       <Sidebar />
-      <div className="p-5">{children}</div>
+      <div className="p-5 max-h-screen overflow-y-auto scrollbar">
+        {children}
+      </div>
     </div>
   )
 }
---
+++ src/pages/dashboard/posts/index.tsx
@@ -0,0 +1,26 @@
+import Dashboard from "layouts/dashboard"
+import Link from "next/link"
+import React from "react"
+import { BiPlus } from "react-icons/bi"
+
+type Props = {}
+
+const DashboardPostsPage = (props: Props) => {
+  return (
+    <Dashboard>
+      <div className="h-[200vh]">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Posts</h1>
+
+          <Link href="/posts/create">
+            <a className="flex items-center gap-2 text-xs px-3 py-2 bg-indigo-500 text-white rounded-md">
+              <BiPlus className="text-lg" /> Create Post
+            </a>
+          </Link>
+        </div>
+      </div>
+    </Dashboard>
+  )
+}
+
+export default DashboardPostsPage
