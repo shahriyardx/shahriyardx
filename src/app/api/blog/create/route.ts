ad031eb58d8630e306a2d3c3a0e6afe62b50ed92
Md Shahriyar Alam <contact@shahriyar.dev>
2023-10-23 04:41:14 +0600

    added protection for create blog

--- src/app/api/blog/create/route.ts
+++ src/app/api/blog/create/route.ts
@@ -2,8 +2,16 @@ import { prisma } from "@/tools/db"
 import { revalidatePath } from "next/cache"
 import { NextResponse, NextRequest } from "next/server"
 import slugify from "slugify"
+import { authOptions } from "../../auth/[...nextauth]/authOptions"
+import { getServerSession } from "next-auth"
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    return NextResponse.json({ success: false, message: "unauthenticated" })
+  }
+
   const body = await req.json()
   const postdata = {
     ...body,
