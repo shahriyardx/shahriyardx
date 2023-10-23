f0151deb1e5572bad52a57a9331d0ae6459a4a88
Md Shahriyar Alam <contact@shahriyar.dev>
2023-10-23 18:00:37 +0600

    removed cards

--- src/app/blog/BlogInfo.tsx
+++ src/app/blog/BlogInfo.tsx
@@ -18,17 +18,12 @@ const BlogInfo = ({
   return (
     <div className={`flex items-center gap-2 sm:gap-5 text-xs sm:text-sm flex-wrap ${className}`}>
       <span className="flex items-center gap-2">
-        <BiUser />
-        <span>shahriyardx</span>
-      </span>
-
-      <span className="flex items-center gap-2">
-        <BiCalendar className="hidden sm:block"/>
+        <BiCalendar />
         {moment(new Date(Number(blog.time))).fromNow()}
       </span>
 
       <span className="flex items-center gap-2">
-        <BiTime className="hidden sm:block"/>
+        <BiTime />
         {moment.duration(time, "minute").humanize()} read
       </span>
     </div>
