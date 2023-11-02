194f25a5f3065579524a715874afd6aab045f312
Md Shahriyar Alam <contact@shahriyar.dev>
2023-11-03 00:40:23 +0600

    pixed image positioning on og

--- src/app/api/blog/og/route.tsx
+++ src/app/api/blog/og/route.tsx
@@ -53,7 +53,7 @@ export async function GET(req: NextRequest) {
             width={100}
             height={100}
             alt="me"
-            tw="rounded-full absolute bottom-5 right-5"
+            tw="rounded-full absolute bottom-[50] right-[50]"
 
           />
       </div>
