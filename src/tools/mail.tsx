f6125efa6de866b4bc940e51a334b10878f5fbae
Md Shahriyar Alam <contact@shahriyar.dev>
2023-11-04 16:46:58 +0600

    removed email deps

--- src/tools/mail.tsx
+++
@@ -1,6 +0,0 @@
-import PH from "../../email/emails/ph"
-import { renderAsync } from "@react-email/render"
-
-export const getHtml = async (content: string) => {
-  return await renderAsync(<PH content={content} />)
-}
