ab04a0abf327aadf9bd6ddd8cb08b33059528d07
Md Shahriyar Alam <contact@shahriyar.dev>
2023-11-07 19:33:37 +0600

    return comment from specific post

--- src/app/api/blog/public/[slug]/comment/route.ts
+++ src/app/api/blog/public/[slug]/comment/route.ts
@@ -4,15 +4,19 @@ import { NextRequest, NextResponse } from "next/server"
 export const dynamic = "force-dynamic"
 
 export const GET = async (
-    req: NextRequest,
-    { params }: { params: { blogId: string } }
-  ) => {
-    const comments = await prisma.comment.findMany({
-      where: { postId: params.blogId },
-      orderBy: {
-        createdAt: "desc",
-      },
-    })
-  
-    return NextResponse.json(comments)
+  req: NextRequest,
+  { params }: { params: { slug: string } }
+) => {
+  const post = await prisma.blogPost.findFirst({ where: { slug: params.slug } })
+  if (!post) {
+    return NextResponse.json({ comments: [] })
   }
+  const comments = await prisma.comment.findMany({
+    where: { postId: post.id },
+    orderBy: {
+      createdAt: "desc",
+    },
+  })
+
+  return NextResponse.json(comments)
+}
