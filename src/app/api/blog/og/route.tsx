50df6181f05c8ef467b0d2689d28bbd43d696f90
Md Shahriyar Alam <contact@shahriyar.dev>
2023-11-16 18:39:41 +0600

    shorten renamed

--- src/app/api/blog/og/route.tsx
+++ src/app/api/blog/og/route.tsx
@@ -1,8 +1,6 @@
 import { ImageResponse, NextRequest } from "next/server"
 import moment from "moment"
 
-export const runtime = "edge"
-
 const regularFont = fetch(
   new URL("/public/fonts/inter-regular.ttf", import.meta.url),
 ).then((res) => res.arrayBuffer())
