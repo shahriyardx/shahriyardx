e84bb8a7c567f09aab44828f164a96b48a278b04
Md Shahriyar Alam <contact@shahriyar.dev>
2023-11-16 18:30:54 +0600

    added shorten

---
+++ src/app/api/shorten /route.ts
@@ -0,0 +1,60 @@
+import { prisma } from "@/tools/db"
+import { NextRequest, NextResponse } from "next/server"
+
+const getRandomTitle = () => {
+  let text = ""
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  const charactersLength = characters.length
+  let counter = 0
+  while (counter < 5) {
+    text += characters.charAt(Math.floor(Math.random() * charactersLength))
+    counter += 1
+  }
+
+  return text
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  let { url, title } = (await req.json()) as { url: string; title?: string }
+  if (!title) title = getRandomTitle()
+
+  const data = await prisma.link.create({
+    data: {
+      url,
+      text: title,
+    },
+  })
+
+  return NextResponse.json(data)
+}
+import { prisma } from "@/tools/db"
+import { NextRequest, NextResponse } from "next/server"
+
+const getRandomTitle = () => {
+  let text = ""
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  const charactersLength = characters.length
+  let counter = 0
+  while (counter < 5) {
+    text += characters.charAt(Math.floor(Math.random() * charactersLength))
+    counter += 1
+  }
+
+  return text
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  let { url, title } = (await req.json()) as { url: string; title?: string }
+  if (!title) title = getRandomTitle()
+
+  const data = await prisma.link.create({
+    data: {
+      url,
+      text: title,
+    },
+  })
+
+  return NextResponse.json(data)
+}
