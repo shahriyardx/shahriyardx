73536b7bbced6610de83df041f5cad5c78039b0f
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2024-07-05 18:41:40 +0600

    removed admin panel and and email

--- src/app/links/[slug]/route.ts
+++ src/app/links/[slug]/route.ts
@@ -4,6 +4,8 @@ import { redirect } from "next/navigation"
 
 import type { NextRequest } from "next/server"
 
+export const dynamic = "force-dynamic"
+
 export async function GET(
 	_req: NextRequest,
 	{ params }: { params: { slug: string } },
