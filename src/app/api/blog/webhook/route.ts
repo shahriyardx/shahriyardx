ab69153b02b25f944667ff03045fee8f43a2df3d
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2024-07-05 16:45:21 +0600

    revalidate slug

--- src/app/api/blog/webhook/route.ts
+++ src/app/api/blog/webhook/route.ts
@@ -3,5 +3,7 @@ import { type NextRequest, NextResponse } from "next/server"
 
 export async function POST(req: NextRequest) {
 	revalidatePath("/blog")
+	revalidatePath("/blog/[slug]")
+
 	return NextResponse.json({ success: true })
 }
