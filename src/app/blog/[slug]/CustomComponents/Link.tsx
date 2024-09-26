b7cbc378c67062cb21d26fd3cb7a74fb1cbb8df7
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2024-09-26 17:03:56 +0600

    fix words

--- src/app/blog/[slug]/CustomComponents/Link.tsx
+++ src/app/blog/[slug]/CustomComponents/Link.tsx
@@ -1,22 +1,21 @@
 import Image from "next/image"
-import React, { type HTMLAttributes } from "react"
+import type React from "react"
 
-type Props = HTMLAttributes<HTMLAnchorElement>
+type Props = React.JSX.IntrinsicElements["a"]
 
-const Link = (props: Props) => {
-	// @ts-expect-error src-exists
-	const origin = new URL(props.href).origin
+const Link = ({ href, children, ...props }: Props) => {
+	const origin = new URL(href as string, process.env.NEXT_PUBLIC_URL).hostname
 
 	return (
-		<a {...props} className="m-0 p-0">
+		<a {...props} href={href} className="m-0 p-0 inline-block">
 			<Image
-				src={`https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${origin}&size=16`}
+				src={`https://icons.duckduckgo.com/ip3/${origin}.ico`}
 				className="w-5 h-5 rounded-full m-0 p-0 inline-block mr-2"
 				alt={origin}
 				width={20}
 				height={20}
 			/>
-			<span className="truncate">{props.children}</span>
+			<span className="break-words">{children}</span>
 		</a>
 	)
 }
