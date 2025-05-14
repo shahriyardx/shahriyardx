31c90af8ad65f4d27ff96395f4133fcb13f8c252
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2025-05-15 01:16:52 +0600

    updated privacy

--- src/app/apps/todx/privacy/page.tsx
+++ src/app/apps/todx/privacy/page.tsx
@@ -2,37 +2,50 @@ import Markdown from "@/app/blog/[slug]/markdown/Markdown"
 import React from "react"
 
 const md_text = `
+**Last Updated:** 15/5/2025
+
 Your privacy is our priority. This Privacy Policy outlines how Todx handles user information and data.
 
 ## 1. Data Collection
-Todx does not collect, store, or process any personal information. We respect your privacy and ensure that your data stays on your device.
 
-## 2. Data Storage
+Todx is designed with your privacy in mind. We **do not collect, store, or transmit any personal information** from your device. We respect your privacy and ensure that your data stays securely on your device.
+
+## 2. App Permissions
+
+**Todx does not request any special permissions to access data or hardware on your device.**
+
+## 3. Data Storage
+
 All tasks and related data created within the app are stored locally on your device. No information is transmitted to or stored on any external servers.
 
-## 3. Third-Party Services
+## 4. Third-Party Services
+
 Todx does not use any third-party services for analytics, advertising, or data collection.
 
-## 4. Authentication
+## 5. Authentication
+
 Todx does not require user authentication, and no personal credentials are collected or stored.
 
-## 5. Data Sharing
+## 6. Data Sharing
+
 Todx does not share any data with third parties. Your information remains private and stays on your device.
 
-## 6. User Data Management
-If you wish to delete your data, you can simply uninstall the app or clear the app’s data from your device settings.
+## 7. User Data Management
 
-## 7. Children’s Privacy
-Todx is not intended for use by children. The app is designed for users aged 13 and above.
+If you wish to delete your data, you can simply uninstall the app or clear the app’s data from your device settings. This will permanently remove all tasks and information stored locally by Todx on your device.
 
-## 8. Changes to This Privacy Policy
+## 8. Children’s Privacy
+Todx is rated for users aged 3+. While the app's content is generally suitable for a wide audience, Todx is designed as a personal productivity tool and is not specifically targeted at children under the age of 13. Due to the nature of the app storing all data locally and not collecting any personal information, we do not knowingly process or have access to any data from children under 13. We encourage parents and guardians to monitor their children’s use of digital content.
+
+## 9. Changes to This Privacy Policy
+
 We may update this privacy policy from time to time. Any changes will be reflected here. We recommend that you review this policy periodically to stay informed of any updates.
 
-----
+------
 
-If you have any questions or concerns about this privacy policy, feel free to contact us at [mdshahriyaralam552@gmail.com](mailto:mdshahriyaralam552@gmail.com).
+If you have any questions or concerns about this privacy policy, feel free to contact us at [mdshahriyaralam552@gmail.com](mailto:mdshahriyaralam552@gmail.com)
 
-----
+------
 `
 
 const Privacy = () => {
--- src/app/blog/[slug]/CustomComponents/Link.tsx
+++ src/app/blog/[slug]/CustomComponents/Link.tsx
@@ -8,13 +8,13 @@ const Link = ({ href, children, ...props }: Props) => {
 
 	return (
 		<a {...props} href={href} className="m-0 p-0 inline-block">
-			<Image
+			{/* <Image
 				src={`https://icons.duckduckgo.com/ip3/${origin}.ico`}
 				className="w-5 h-5 rounded-full m-0 p-0 inline-block mr-2"
 				alt={origin}
 				width={20}
 				height={20}
-			/>
+			/> */}
 			<span className="break-words">{children}</span>
 		</a>
 	)
