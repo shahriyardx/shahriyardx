1da2c1e7956e94e8985168979fbd65c453ee5f32
Shahriyar Alam <mdshahriyaralam552@gmail.com>
2026-01-23 13:24:19 +0600

    fix outdated classes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-}
-
-export default config
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+}
+
+export default config;
+
